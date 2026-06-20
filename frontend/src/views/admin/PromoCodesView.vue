deef85aa1033c8a7f639a4356c4d9ca41ef9f2f4
KimiChen <jianxieshui@gmail.com>
2026-06-21 07:01:43 +0800

    fix: expand promo codes table naturally

--- frontend/src/views/admin/PromoCodesView.vue
+++ frontend/src/views/admin/PromoCodesView.vue
@@ -1,6 +1,6 @@
 <template>
   <AppLayout>
-    <TablePageLayout>
+    <TablePageLayout natural-table-height>
       <template #filters>
         <div class="flex flex-wrap items-center gap-3">
           <!-- Left: Search + Filters -->
@@ -44,6 +44,7 @@
           :data="codes"
           :loading="loading"
           :server-side-sort="true"
+          :virtualized="false"
           default-sort-key="created_at"
           default-sort-order="desc"
           @sort="handleSort"
